fe96d281431ccf256eb77a3cad3f59832d043e04
Payam Zahedi <pzahedi1995@gmail.com>
2023-08-22 16:44:09 +0330

    add raw custom drop down

--- lib/custom_drop_down.dart
+++ lib/custom_drop_down.dart
@@ -1,7 +1,3 @@
 library custom_drop_down;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/widget/raw.dart';
---
+++ lib/src/widget/raw.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+typedef ButtonBuilder = Widget Function(
+  BuildContext context,
+  VoidCallback onTap,
+);
+
+typedef MenuBuilder = Widget Function(
+  BuildContext context,
+  double? width,
+);
+
+enum MenuPosition {
+  top,
+  bottom,
+}
+
+class RawCustomDropDown extends StatefulWidget {
+  const RawCustomDropDown({
+    super.key,
+    required this.controller,
+    required this.buttonBuilder,
+    required this.menuBuilder,
+    this.menuPosition = MenuPosition.bottom,
+  });
+
+  final OverlayPortalController controller;
+
+  final ButtonBuilder buttonBuilder;
+  final MenuBuilder menuBuilder;
+  final MenuPosition menuPosition;
+
+  @override
+  State<RawCustomDropDown> createState() => _RawCustomDropDownState();
+}
+
+class _RawCustomDropDownState extends State<RawCustomDropDown> {
+  final _link = LayerLink();
+
+  /// width of the button after the widget rendered
+  double? _buttonWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _link,
+      child: OverlayPortal(
+        controller: widget.controller,
+        overlayChildBuilder: (BuildContext context) {
+          return CompositedTransformFollower(
+            link: _link,
+            targetAnchor: Alignment.bottomLeft,
+            showWhenUnlinked: false,
+            child: Align(
+              alignment: AlignmentDirectional.topStart,
+              child: widget.menuBuilder(context, _buttonWidth),
+            ),
+          );
+        },
+        child: widget.buttonBuilder(context, onTap),
+      ),
+    );
+  }
+
+  void onTap() {
+    _buttonWidth = context.size?.width;
+
+    widget.controller.toggle();
+  }
+}
